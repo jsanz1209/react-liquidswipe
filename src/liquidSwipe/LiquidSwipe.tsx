34f083d2984b423ab7068211e46067f75507084d
jsanz1209 <jsanz1209@gmail.com>
2023-01-19 15:06:20 +0100

    Update LiquidSwipe.tsx

--- src/liquidSwipe/LiquidSwipe.tsx
+++ src/liquidSwipe/LiquidSwipe.tsx
@@ -3,13 +3,14 @@ import { useRef, useState, useEffect, FC } from 'react';
 import Page from './Page';
 import { LQSW_CONTAINER } from './Styled';
 
-export default function LiquidSwipe({ components, style }: { components: FC[], style?: any }): ReactElement {
+export default function LiquidSwipe({ components, onChangeIndex, style }: { components: FC[], onChangeIndex: (index: number) => void, style?: any }): ReactElement {
   const sizeOfSwipe = components.length;
   const [isActive, setActive] = useState(0);
   const [elm, setElm] = useState<ReactElement>();
   const parentElement = useRef<HTMLDivElement>(null);
 
   useEffect(() => {
+    onChangeIndex(index);
     setElm(
       <Page
         key={isActive}
